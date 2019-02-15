11cc29fc0995c3946d28976f1dc49789d3eb0b9e
Nishant Sahay <37246425+NishantSahay7@users.noreply.github.com>
2019-02-16 03:50:21 +0530

    Add files via upload

---
+++ Video.bat
@@ -0,0 +1,3 @@
+echo
+start https://www.youtube.com/watch?v=RNehg6AKCiM&t=636s
+echo
---
+++ install_mysql_server.bat
@@ -0,0 +1,3 @@
+echo
+start https://howchoo.com/g/mtm3zdq2nzv/how-to-add-a-mysql-user-and-grant-privileges
+echo
